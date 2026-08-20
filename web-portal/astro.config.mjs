588d85af25adf7b951c7e55fa38ead3cc94bfbb7
Tony <tony@localvault.com>
2026-08-20 06:09:52 -0700

    Configure GitHub Pages base path routing and deployment workflow

--- web-portal/astro.config.mjs
+++ web-portal/astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://anthonyferrar.github.io',
+  base: '/the-interwingled-memex',
   integrations: [tailwind()],
   server: {
     port: 3000
